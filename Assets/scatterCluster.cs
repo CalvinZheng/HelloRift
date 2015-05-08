ece258dafa84bd9cdfc91a345d7e478589b00f6a
CalvinZheng <calvinz.mcgill@gmail.com>
2015-05-08 17:43:35 -0400

    add red cube, resized the scene

--- Assets/scatterCluster.cs
+++ Assets/scatterCluster.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class scatterCluster : MonoBehaviour {
 
 	public Transform fragment;
+	public Transform redCube;
 	public int fragCount;
 
 	private Transform[] cluster;
@@ -14,12 +15,17 @@ public class scatterCluster : MonoBehaviour {
 		Random.seed = (int)System.DateTime.Now.Ticks;
 
 		cluster = new Transform[fragCount];
-		for (int i = 0; i < 100; i++)
+		for (int i = 0; i < fragCount; i++)
 		{
 			cluster[i] = Instantiate(fragment) as Transform;
 			cluster[i].position = new Vector3(Random.value*10-5, Random.value*10, Random.value*10);
 			cluster[i].rotation = Random.rotation;
 		}
+
+		Transform redCube1 = Instantiate (redCube) as Transform;
+		redCube1.position = new Vector3 (-Random.value * 5, Random.value * 10, Random.value * 10);
+		Transform redCube2 = Instantiate (redCube) as Transform;
+		redCube2.position = new Vector3 (Random.value * 5, Random.value * 10, Random.value * 10);
 	}
 	
 	// Update is called once per frame
